3fef304da273fb716eb825ebc8286c0c8c56a6f2
Logan Martin <88mlogan@gmail.com>
2023-11-09 15:14:39 -0500

    Complete Final

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+console.log("isOnSale");
+console.log("inStock");
 
 // Exercise 1
 let lastName = "Martin";
-var age = "21";
+var age = 21;
 
 // Exercise 2
 const firstName = "Logan";
 
 // Exercise 3
 let language = "JavaScript";
-let createdYear = "1995";
-let inCaseSensitive = "true";
+let createdYear = 1995;
+let inCaseSensitive = true;
 
 // Exercise 4
-let price = "19.99";
-let isnOnSale = "false";
-let salePercentaage = "15";
-let stock = "0";
-let inStock = "false";
+let price = 19.99;
+let isOnSale = false;
+let salePercentaage = 15;
+let stock = 0;
+let inStock = false;
 let selectedSize = "M";
 
 // Exercise 5
 let title = "Name of the Wind";
 let author = "Patrick Rothfuss";
-let pageCount = "722";
-let bookmark = "456";
-let hasRead = "true";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
