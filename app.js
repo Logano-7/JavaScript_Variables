abfd7f778a0a6eb039f1323f83f7dac05ef0b1a3
Logan Martin <88mlogan@gmail.com>
2023-11-09 13:00:19 -0500

    Complete

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ console.log(
 );
 
 // Exercise 1
+let lastName = "Martin";
+var age = "21";
 
 // Exercise 2
+const firstName = "Logan";
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = "1995";
+let inCaseSensitive = "true";
+
+// Exercise 4
+let price = "19.99";
+let isnOnSale = "false";
+let salePercentaage = "15";
+let stock = "0";
+let inStock = "false";
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = "722";
+let bookmark = "456";
+let hasRead = "true";
